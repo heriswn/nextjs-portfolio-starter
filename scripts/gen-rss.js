2190826177b37ff1aceb5fc712940507e835a067
헤리 세티아완 <48311762+heriswn@users.noreply.github.com>
2022-11-19 00:59:15 +0000

    setting up blog

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Heri Setiawan',
+    site_url: 'https://opini.vercel.app',
+    feed_url: 'https://opini.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
